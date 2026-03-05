cceec86e4f584987b8265731f31d9d0ad73b1a4c
PoLLi <1657076+poLLi@users.noreply.github.com>
2026-03-05 11:22:26 +0100

    Implement player class and circle shape base class; update main game loop to include player drawing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
@@ -9,7 +10,10 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     while True:
         log_state()
 
@@ -18,8 +22,12 @@ def main():
                 return
         
         screen.fill("black")
+
+        player.draw(screen)
+
         pygame.display.flip()
 
-
+        dt = clock.tick(60) / 1000
+        
 if __name__ == "__main__":
     main()
