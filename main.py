564b4c8764a99cbe5799e741e59cdcddbcd1efd2
PoLLi <1657076+poLLi@users.noreply.github.com>
2026-03-05 12:11:24 +0100

    Add asteroid and asteroid field classes; update main loop for asteroid spawning and management

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
-from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -13,18 +16,35 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    field = AsteroidField()
+
+
     while True:
         log_state()
 
+        screen.fill("black")
+
+        # EVENT HANDLING
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill("black")
+        # UPDATE Group
+        for obj in updatable:
+            obj.update(dt)
 
-        player.update(dt)
-        player.draw(screen)
+        # DRAW Group
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
 
